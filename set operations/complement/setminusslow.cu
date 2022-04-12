cc5e110a66a097e56287dd83aece01d87d023a6e
pranjicb <55251368+pranjicb@users.noreply.github.com>
2022-04-12 11:17:16 +0200

    Add files via upload

---
+++ set operations/complement/setminusslow.cu
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+
+__global__ void setminus(int n, bool *x, bool *y){
+    for(int i = 0; i < n; i++){
+        x[i] = x[i] & !y[i];
+    }
+}
+
+
+int main(void){
+
+    int N = 1<<20;
+    bool *x, *y, *z;
+
+    cudaMallocManaged(&x, N*sizeof(bool));
+    cudaMallocManaged(&y, N*sizeof(bool));
+    cudaMallocManaged(&z, N*sizeof(bool));
+
+    for(int i = 0; i < N; i++){
+        x[i] = rand() % 2;
+        y[i] = rand() % 2;
+        z[i] = x[i] & !y[i];
+    }
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    setminus<<<1,1>>>(N,x,y);
+    cudaDeviceSynchronize();
+
+    int count = 0;
+    for(int i = 0; i < N; i++){
+        count += x[i] ^ z[i];
+    }
+
+    std::cout << "Error: " << count << std::endl;
+
+    cudaFree(x);
+    cudaFree(y);
+    cudaFree(z);
+
+}

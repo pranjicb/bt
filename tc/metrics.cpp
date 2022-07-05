89ce728f4d2148abfddc7794d8a4f6cf9d958c53
pranjicb <pranjicb@student.ethz.ch>
2022-07-05 12:42:34 +0200

    analysis of metrics

--- tc/metrics.cpp
+++ tc/metrics.cpp
@@ -6,46 +6,118 @@
 #include <algorithm>
 
 int main(){
-    GraphReader graph = BNMOUSE;
-    std::string filename = graph.name + ".csv";
-    std::ofstream out(filename);
+
+#pragma omp parallel num_threads(16)
+{
+
+    std::ofstream out("metrics.txt");
     std::streambuf *coutbuf = std::cout.rdbuf();
     std::cout.rdbuf(out.rdbuf());
-    std::ifstream INPUT(graph.filepath);    
-    std::vector<unsigned int> degs(graph.N, 0);
-    unsigned int N = graph.N;
-    unsigned int E = graph.E;
-    unsigned int maxdeg = 0;
-    unsigned long long n2 = 1ULL * N * N;
-    double density = n2/(1.0*E);
-    double neRatio = N/(1.0*E);
-    double enRatio = E/(1.0*N);
-    unsigned long long degsum = 0;
-    double avgdeg;
+    std::cout << "Name, nodes, edges, density, max degree, avg degree, mode, sigma, median" << std::endl;
+    #pragma omp for
+    for(unsigned int it = 0; it < testGraphs.size(); it++){
+        GraphReader graph = testGraphs.at(it);
+        
+        std::ifstream INPUT(graph.filepath);    
+        std::vector<unsigned int> degs(graph.N, 0);
+        unsigned int N = graph.N;
+        unsigned int E = graph.E;
+        unsigned int maxdeg = 0;
+        unsigned long long n2 = 1ULL * N * N;
+        double density = 1.0*E/n2;
+        unsigned long long degsum = 0;
+        double avgdeg = 0.0;
+        unsigned int median = 0;
+        unsigned int modalwert = 0;
     
-    unsigned int u,v;
-    std::string w;
-    while(INPUT >> u >> v){
-        if(u < v){
-            degs.at(u)++;
-            degs.at(v)++;
+        unsigned int u,v;
+        std::string w;
+
+        if(graph.type==0){
+            while(INPUT >> u >> v >> w){
+                if(u < v){
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+                if(v < u){
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+            }
         }
-        else if(v < u){
-            degs.at(u)++;
-            degs.at(v)++;
+        else if(graph.type==1){
+            while(INPUT >> u >> v >> w){
+                if(u < v){
+                    --u;
+                    --v;
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+                if(v < u){
+                    --u;
+                    --v;
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+            }
         }
-    }
-    for(unsigned int i = 0; i < N; i++){
-        degsum += degs.at(i);
-        if(maxdeg < degs.at(i)){
-            maxdeg = degs.at(i);
+        else if(graph.type==2){
+            while(INPUT >> u >> v){
+                if(u < v){
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+                if(v < u){
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+            }
         }
+        else{
+            while(INPUT >> u >> v){
+                if(u < v){
+                    --u;
+                    --v;
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+                if(v < u){
+                    --u;
+                    --v;
+                    degs.at(u)++;
+                    degs.at(v)++;
+                }
+            }
+        }
+        
+        for(unsigned int i = 0; i < N; i++){
+            degsum += degs.at(i);
+            if(maxdeg < degs.at(i)){
+                maxdeg = degs.at(i);
+            }
+        }
+        avgdeg = degsum*1.0/N;
+        std::vector<unsigned int> histogram(maxdeg+1, 0);
+        for(unsigned int i = 0; i < graph.N; i++){
+            unsigned int tmp = degs.at(i);
+            histogram.at(tmp)++;
+        }
+        
+        modalwert = std::distance(histogram.begin(), std::max_element(histogram.begin(), histogram.end()));
+        unsigned int sigma = 0;
+        for(unsigned int i = 0; i < N; i++){
+            sigma += (degs.at(i) - avgdeg)*(degs.at(i) - avgdeg);
+        }
+        sigma /= (N-1);
+
+        auto m = degs.begin() + degs.size()/2;
+        std::nth_element(degs.begin(), m, degs.end());
+        median = degs.at(degs.size()/2);
+
+        
+
+        std::cout << graph.name << "," << N << "," << E << "," << density << "," << maxdeg << "," << avgdeg << "," << modalwert << "," << sigma << "," << median << std::endl;
     }
 
-    avgdeg = degsum*1.0/N;
-
-    std::cout << N << "," << E << "," << maxdeg << "," << avgdeg << "," << density << "," << neRatio << "," << enRatio << "," << std::endl;
-    for(unsigned int j = 0; j < N; j++){
-        std::cout << j << "," << degs.at(j) << std::endl;
-    }
+}
 }
